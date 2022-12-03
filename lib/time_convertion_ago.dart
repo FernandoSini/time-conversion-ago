cfd2e2d8c322518d1793fa7cf852568a91408b08
Fernando Fazio Sinigaglia <fernandosini@MacBook-Air-de-Fernando.local>
2022-12-02 22:11:34 -0300

    init version

---
+++ lib/time_convertion_ago.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+library time_convertion_ago;
+
+class TimeConvertionAgo {
+  
+  String convertDate(DateTime postedAt) {
+    if (DateTime.now().difference(postedAt).inSeconds < 60) {
+      return "${DateTime.now().difference(postedAt).inSeconds} seconds ago";
+    } else {
+      if (DateTime.now().difference(postedAt).inSeconds <= 3600) {
+        return "${DateTime.now().difference(postedAt).inMinutes} minutes ago";
+      }
+      if (DateTime.now().difference(postedAt).inSeconds <= 86400) {
+        return "${DateTime.now().difference(postedAt).inHours} hours ago";
+      } else {
+        return "${DateTime.now().difference(postedAt).inDays} days ago";
+      }
+    }
+  }
+}
---
+++ test/time_convertion_ago_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:time_convertion_ago/time_convertion_ago.dart';
+
+void main() {
+  test(
+    'Convert datetime to ago',
+    () {
+      final convertion = TimeConvertionAgo();
+      final DateTime? date = DateTime.tryParse("2022-11-30T21:21:09.32Z");
+      expect(convertion.convertDate(date!), convertion.convertDate(date));
+      expect(convertion.convertDate(date), convertion.convertDate(date));
+      expect(convertion.convertDate(date), convertion.convertDate(date));
+      expect(convertion.convertDate(date), convertion.convertDate(date));
+      print(convertion.convertDate(date));
+      /* expect(convertion.convertDate(date),
+          "${DateTime.now().difference(dateInDays).inDays} days ago"); */
+    },
+  );
+}
