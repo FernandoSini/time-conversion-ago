9480f71e5f424c92858282e6dbf623a92ef86c58
Fernando Fazio Sinigaglia <fernandosini@MacBook-Air-de-Fernando.local>
2022-12-03 12:33:18 -0300

    refactoring package name

--- lib/time_conversion_ago.dart
+++ lib/time_conversion_ago.dart
@@ -1,8 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 library time_convertion_ago;
 
-class TimeConvertionAgo {
-  
+class TimeConversionAgo {
   String convertDate(DateTime postedAt) {
     if (DateTime.now().difference(postedAt).inSeconds < 60) {
       return "${DateTime.now().difference(postedAt).inSeconds} seconds ago";
--- test/time_conversion_ago_test.dart
+++ test/time_conversion_ago_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:time_convertion_ago/time_convertion_ago.dart';
+import 'package:time_conversion_ago/time_conversion_ago.dart';
 
 void main() {
   test(
     'Convert datetime to ago',
     () {
-      final convertion = TimeConvertionAgo();
+      final convertion = TimeConversionAgo();
       final DateTime? date = DateTime.tryParse("2022-11-30T21:21:09.32Z");
       expect(convertion.convertDate(date!), convertion.convertDate(date));
       expect(convertion.convertDate(date), convertion.convertDate(date));
